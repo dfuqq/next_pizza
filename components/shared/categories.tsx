e33433c16181b583b11ef9707804c77a39b8d518
dfuqq <dan38449@mail.ru>
2024-10-31 03:28:07 +0500

    seeding for db

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -16,7 +16,6 @@ const categories = [
 	{ id: 5, name: 'Кофе' },
 	{ id: 6, name: 'Напитки' },
 	{ id: 7, name: 'Десерты' },
-	{ id: 8, name: 'Десерты' },
 ];
 
 export const Categories: React.FC<Props> = ({ className }) => {
